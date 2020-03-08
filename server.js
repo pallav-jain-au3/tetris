c39e76356d5c81ae4cfd845d5caf75f447183d9f
Pallav Jain <pallavjain409@gmail.com>
2020-03-08 17:22:32 +0530

    server started

--- server.js
+++ server.js
@@ -2,13 +2,25 @@ const express = require('express');
 const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
+const router = require('./server/index.js')
 app.use(express.static(__dirname));
-console.log("a",__dirname)
-app.use(express.static(path.join(__dirname, 'client/build')));
-app.get('/ping', function (req, res) {
- return res.send('pong');
-});
+const mongoose = require('mongoose')
+require("dotenv").config();
+
+app.use(express.static(path.join(__dirname, 'client/build/static')));
+app.use(express.json());
+
+
+mongoose.connect(
+  process.env.DB_HOST,
+  {
+    useNewUrlParser: true
+  },
+  () => console.log("connected to database")
+);
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
+
+app.use('/api', router)
 app.listen(port, () => console.log("listenning on 8080"));
--- server/index.js
+++ server/index.js
@@ -1,8 +1,6 @@
 const express = require("express");
-const app = express();
-const PORT = process.env.PORT || 5000;
-const mongoose = require("mongoose");
-require("dotenv").config();
+const router = express.Router();
+const mongoose = require('mongoose')
 const {
   registerUser,
   loginUser,
@@ -11,24 +9,11 @@ const {
 
 const {addScore, getScore} = require('./handlers/ScoresHandle')
 const auth = require("./auth");
-const cors = require("cors");
-
-app.use(cors());
-app.use(express.json());
-console.log()
-mongoose.connect(
-  process.env.DB_HOST,
-  {
-    useNewUrlParser: true
-  },
-  () => console.log("connected to database")
-);
-
 // routes
-app.post("/user/register", registerUser);
-app.post("/user/login", loginUser);
-app.get("/user/auth", auth, getAuthenticatedUser);
-app.post("/score",auth,addScore )
-app.get('/scores', getScore)
+router.post("/user/register", registerUser);
+router.post("/user/login", loginUser);
+router.get("/user/auth", auth, getAuthenticatedUser);
+router.post("/score",auth,addScore )
+router.get('/scores', getScore)
+module.exports = router
 
-app.listen(PORT, () => console.log(`listenning on port ${PORT}`));
