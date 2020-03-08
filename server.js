f40f8620b04eebbf892e17581767207b16b8d51c
Pallav Jain <pallavjain409@gmail.com>
2020-03-08 15:34:23 +0530

    deploye

--- server.js
+++ server.js
@@ -9,6 +9,6 @@ app.get('/ping', function (req, res) {
  return res.send('pong');
 });
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 app.listen(port, () => console.log("listenning on 8080"));
